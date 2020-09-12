7b49b792caba3153586b4c30ed5dd98e82d19222
AnthonyKwon <me@thonlog.com>
2020-09-12 06:52:33 +0000

    Implemented String Resolver.

--- modules/commands/info.js
+++ modules/commands/info.js
@@ -1,9 +1,11 @@
+const string = require('../stringResolver');
+
 module.exports = {
     name: 'info',
-    argsRequired: false,
-    aliases: ['정보'],
-    description: 'Catty에 대한 정보를 알려줍니다.',
+    argsRequired: true,
+    aliases: [string.get('infoCommandAliases')],
+    description: string.get('infoCommandDesc'),
     execute(message, args) {
-        message.channel.send('정보: 현재 내용이 없습니다.');
+        message.channel.send('NO_MESSAGE');
     },
 };
