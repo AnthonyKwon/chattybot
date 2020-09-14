5b4379edcf07081df3ee8c8fe89177e362ce26dd
Anthony Kwon <12590757+AnthonyKwon@users.noreply.github.com>
2020-09-14 23:44:09 +0000

    Added bot info, changed say filter mechanism.

--- modules/commands/info.js
+++ modules/commands/info.js
@@ -1,4 +1,6 @@
 const string = require('../stringResolver');
+const { version } = require('../../package.json');
+const repository = 'https://github.com/AnthonyKwon/discord-catty/';
 
 module.exports = {
     name: string.get('infoCommandName'),
@@ -7,6 +9,10 @@ module.exports = {
     description: string.get('infoCommandDesc').format(string.get('localizedBotName')),
     execute(message, args) {
         const info_message = [];
+        info_message.push(string.get('infoMessage1').format(string.get('localizedBotName'), version));
+        info_message.push(string.get('infoMessage2').format(string.get('localizedBotName')));
+        info_message.push(string.get('infoMessage3'));
+        info_message.push(string.get('infoMessage4').format(repository));
         message.channel.send(info_message);
     },
 };
