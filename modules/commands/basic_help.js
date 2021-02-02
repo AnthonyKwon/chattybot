ca25f6f809d9109fa02c32835b6b73f7d6a144e1
AnthonyKwon <me@thonlog.com>
2021-02-02 08:08:40 +0000

    Made changes all over to deal with the new name.

--- modules/commands/basic_help.js
+++ modules/commands/basic_help.js
@@ -8,27 +8,27 @@ function commandHelp(message, args) {
     const reply = [];
     if (args.length === 0) {
         /* If no argument provided, send full help to DM */
-        reply.push(string.stringFromId('catty.help.message.main.line1'));
-        reply.push(string.stringFromId('catty.help.message.main.line2'));
+        reply.push(string.stringFromId('chattybot.help.message.main.line1'));
+        reply.push(string.stringFromId('chattybot.help.message.main.line2'));
         reply.push('*' + message.client.commands.map(cmd => '%' + string.stringFromId(cmd.name)).join(', ') + '*');
-        reply.push('\n' + string.stringFromId('catty.help.message.main.line3',
+        reply.push('\n' + string.stringFromId('chattybot.help.message.main.line3',
             config.prefix, string.stringFromId(this.name), string.stringFromId(this.usage)));
         try {
             const resposne = message.author.send(reply, {split:true});
-            message.channel.send(string.stringFromId('catty.help.message.check_dm', message.author));
+            message.channel.send(string.stringFromId('chattybot.help.message.check_dm', message.author));
         } catch(err) {
             logger.log('info', `[discord.js] Failed to send DM to ${message.author.tag}: ${err}\n${err.body}\n${err.stack}`);
-            message.channel.send(string.stringFromId('catty.help.message.dm_failed', message.author));
+            message.channel.send(string.stringFromId('chattybot.help.message.dm_failed', message.author));
         }
     }
 }
 
 module.exports = {
-    name: 'catty.command.help',
-    description: 'catty.command.help.desc',
+    name: 'chattybot.command.help',
+    description: 'chattybot.command.help.desc',
     argsRequired: false,
-    aliases: 'catty.command.help.aliases',
-    usage: 'catty.command.help.usage',
+    aliases: 'chattybot.command.help.aliases',
+    usage: 'chattybot.command.help.usage',
     cooldown: 5,
     execute: commandHelp
 }
