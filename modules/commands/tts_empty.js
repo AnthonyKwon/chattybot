58c91d6c2445f00c623cf7a914df7a8cce1d0c8a
Cloud User <12590757+AnthonyKwon@users.noreply.github.com>
2021-02-05 03:45:20 +0000

    Implemented experimental youtube search support.

--- modules/commands/tts_empty.js
+++ modules/commands/tts_empty.js
@@ -1,6 +1,7 @@
 const join = require('./basic_join.js');
 const discord = require('../discord.js');
 const string = require('../stringManager.js');
+const PlayerClass = require('../player.js');
 const TTSClass = require('../textToSpeech.js');
 
 async function ttsEmpty(message) {
@@ -12,9 +13,22 @@ async function ttsEmpty(message) {
 
     /* Re-initialize TTSClass with empty queue */
     voice.TTS = new TTSClass([{ authorId: 0, message: string.stringFromId('chattybot.tts.queue.empty.speak') }]);
+    /* If music is playing, destroy it first */
+    let playtime = 0, queue = [], voiceDestroyed = false;
+    if (voice.Player) {
+        playtime = voice.Player.getPlaytime(voice);
+        queue = voice.Player.queue;
+        voice.Player.stop(voice);
+        voiceDestroyed = true;
+    }
     /* Notify to user */
     message.channel.send(string.stringFromId('chattybot.tts.queue.empty.message'));
     await voice.TTS.speak(message);
+    if (voiceDestroyed === true) {
+        voice.Player = new PlayerClass(queue);
+        voice.Player.play(voice, playtime);
+        destroyed = false;
+    }
 }
 
 module.exports = {
