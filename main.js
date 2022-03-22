a5cdbb7f881e60fe5439917d3847c288604ee833
AnthonyKwon <me@thonlog.com>
2022-03-22 18:02:23 +0900

    unified development/maintenance uses

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const wrapper_init = require('./module/discordwrapper/main.mod');
 const logger = require('./module/logger.js');
 
-const devFlag = process.env.NODE_ENV === 'development' ? true : false;
+const devFlag = process.env.NODE_ENV === 'maintenance' ? true : false;
 
 /* Logging winston to console on development mode. */
 if (devFlag === true) {
