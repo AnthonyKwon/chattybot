2956489281e1b510e839ba7f3e748394117d0e95
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2023-03-08 08:51:15 +0900

    cleanup & switch discord.js to v14 (incomplete)

--- module/discordwrapper/main.mod.js
+++ module/discordwrapper/main.mod.js
@@ -1,22 +1,35 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const slash = require('./slashCommand.js');
 const config = require('../config.js');
-const logger = require('../logger.js');
-const onCommand = require('./command');
+const logger = require('../logger/main.mod.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds
+]});
 
-client.once('ready', () => {
+client.once(Events.ClientReady, c => {
     // create voice session map
-    client.voice.session = new Map(); 
-    logger.log('verbose', `[discord.js] Connected to ${client.user.username}!`);
+    logger.info('discord.js', `Connected to ${client.user.username}!`);
+    c.voice.session = new Map();
+    c.user.setActivity(' — /help');
+
+    // register slash commands
+    logger.info('discord.js', 'Reloading slash commands...');
+    slash.register(config.token, c);
+
+    // load registered slash commands
+    slash.load(c);
 });
 
-client.on('messageCreate', async message => {
-    await onCommand(message);
+client.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand()) return; // filter only for chat interaction
+    slash.handler(interaction);
 });
 
 async function init() {
     await client.login(config.token);
 }
 
-module.exports = init;
+module.exports = { 
+    init
+};
