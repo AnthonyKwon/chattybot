3182787f122c309166f0cb10089a68404983a1a9
AnthonyKwon <me@thonlog.com>
2023-03-07 22:13:53 +0900

    Cleaned up logger code.

--- main.js
+++ main.js
@@ -2,13 +2,4 @@ const winston = require('winston');
 const wrapper_init = require('./module/discordwrapper/main.mod');
 const logger = require('./module/logger.js');
 
-const devFlag = process.env.NODE_ENV === 'maintenance' ? true : false;
-
-/* Logging winston to console on development mode. */
-if (devFlag === true) {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple(),
-    }));
-}
-
 wrapper_init();
--- module/logger.js
+++ module/logger.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const winston = require('winston');
 
+// initialize logger
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
@@ -12,5 +13,14 @@ const logger = winston.createLogger({
         new winston.transports.File({ filename: path.join(__dirname, '../logs/verbose.log'), level: 'verbose' })
     ]
 });
+
+// test if development mode is set
+if (process.env.DEVMODE == 1) {
+    logger.isDevMode = true;
+    logger.add(new winston.transports.Console({ format: winston.format.cli(), level: 'verbose' }));
+} else {
+    logger.isDevMode = false;
+    logger.add(new winston.transports.Console({ format: winston.format.cli(), level: 'warn' }));
+}
 
 module.exports = logger;
