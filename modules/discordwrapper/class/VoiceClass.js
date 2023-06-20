6f2d2dc0680888ab252fe5da4ecabe8f52b8365c
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2023-06-21 03:59:58 +0900

    voice: simplify code

--- modules/discordwrapper/class/VoiceClass.js
+++ modules/discordwrapper/class/VoiceClass.js
@@ -1,83 +1,64 @@
-const path = require('node:path');
 const voice = require('@discordjs/voice');
-const logger = require(path.join(path.dirname(require.main.filename), 'modules', 'logger', 'main.mod.js'));
 
 class VoiceClass {
     constructor(guildId) {
-        this._connection = undefined;
-        this._channel = {
-            id: 0,
-            name: undefined
-        }
         this._guildId = guildId;
         this._tts = undefined;
-        this._volume = 100;
     }
 
+    // get channel id
     get channel() {
-        return this._channel;
+        // get bot's current voice connection on guild
+        const connection = voice.getVoiceConnection(this.guildId);
+        return connection.joinConfig.channelId;
     }
-
-    /* get guild ID of class */
-    get guildId() {
-        return this.guildId;
-    }
-
+    
     // get-set TTS class
-    get TTS() {
-        return this._tts;
-    }
-    set TTS(value) {
-        this._tts = value;
-    }
+    get TTS()  { return this._tts; }
+    set TTS(value)  { this._tts = value; }
 
-    /* get-set voice volume */
-    get volume() {
-        return this._volume;
-    }
-    set volume(rate) {
-        if (rate <= 200 && rate >= 0) {
-            this._volume = rate;
-            if (this._connection.dispatcher)
-                this._connection.dispatcher.setVolume(this._volume / 100);
-        }
-    }
-
-    // join discord voice connection
+    // join into specified voice channel
     async join(channel) {
         // create voice connection and wait for it
-        this._connection = await voice.joinVoiceChannel({
+        const connection = await voice.joinVoiceChannel({
             channelId: channel.id,
             guildId: channel.guild.id,
             adapterCreator: channel.guild.voiceAdapterCreator
         });
-        await voice.entersState(this._connection, voice.VoiceConnectionStatus.Ready, 5_000);
+        await voice.entersState(connection, voice.VoiceConnectionStatus.Ready, 5_000);
 
         // set speaking status to none
-        this._connection.setSpeaking(0);
-        // set channel info
-        this._channel.id = channel.id;
-        this._channel.name = channel.name;
+        connection.setSpeaking(0);
+
         return true;
-        
     }
 
-    play(stream, option=undefined) {
+    // play voice stream to voice channel
+    play(stream) {
+        // get bot's current voice connection on guild
+        const connection = voice.getVoiceConnection(this.guildId);
+
         //TODO: check if bot joined voice channel first
+        if (!connection) return false;
+
         const player = voice.createAudioPlayer({ behaviors: { noSubscriber: voice.NoSubscriberBehavior.Stop }});
         const resource = voice.createAudioResource(stream, { inputType: voice.StreamType.OggOpus });
 
-        this._connection.subscribe(player);
+        connection.subscribe(player);
         player.play(resource);
 
-        //TODO: set volume
         return voice.entersState(player, voice.AudioPlayerStatus.Playing, 5_000);
     }
 
-    /* leave discord voice connection */
+    // leave from currently joined voice channel
     async leave() {
-        //TODO: check if bot join voice channel first
-        this._connection.destroy();
+        // get bot's current voice connection on guild
+        const connection = voice.getVoiceConnection(this.guildId);
+
+        //TODO: check if bot joined voice channel first
+        if (!connection) return false;
+
+        connection.destroy();
         return true;
     }
 }
