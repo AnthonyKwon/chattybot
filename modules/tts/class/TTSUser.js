1946bbffdc91ea9e7766ce15579eb1468d423757
AnthonyKwon <me@thonlog.com>
2023-06-22 01:26:47 +0900

    tts: workaround for discord name changes
    We will get global name directly from API for now.
    This must be reverted when globalname implemented in discord.js

--- modules/tts/class/TTSUser.js
+++ modules/tts/class/TTSUser.js
@@ -1,22 +1,44 @@
+const { getGlobalName } = require('../../discordutils/globalname.js');
+
 // Get server username from user's ID
-const getGuildUsername = (guild, userId) => {
-	const username = guild.members.cache.get(userId).displayName;
-    return username.split('_').join(' ');
+const getUsername = (user, guild=undefined) => {
+    let username = undefined;
+    if (guild) {
+        // get user display name from guild
+	    username = guild.members.cache.get(user.id).displayName;
+        username = username.split('_').join(' ')
+    } else {
+        // get user display name directly
+        username = getGlobalName(user);
+    }
+    return username;
 }
 
 //TODO: change this class to type
 class TTSUserClass {
     constructor(discordUser, guild=undefined) {
-        this._id = discordUser.id;
+        this._user = discordUser;
         // set name field to guild displayname if guild variable is provided
         // set to global displayname if not
-        this._name = guild ? getGuildUsername(guild, discordUser.id) : discordUser.name;
+        this._name = guild ? getUsername(discordUser, guild) : discordUser.name;
     }
 
     // (getter) get TTS user id
-    get id()  { return this._id; }
-    // (getter) get TTS user name
-    get name()  { return this._name; }
+    get id()  { return this._user.id; }
+
+    // get TTS user name
+    async getUsername() { 
+        let username = undefined;
+        if (this._guild) {
+            // get user display name from guild
+            username = this._guild.members.cache.get(user.id).displayName;
+            username = username.split('_').join(' ')
+        } else {
+            // get user display name directly
+            username = getGlobalName(this._user);
+        }
+        return username;
+     }
 }
 
 module.exports = TTSUserClass;
