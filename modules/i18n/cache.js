ae4b2a2f30fb786a49fc5bc9a9336a119210a3fc
AnthonyKwon <me@thonlog.com>
2023-07-02 17:33:02 +0900

    i18n: re-implemented get with cache

---
+++ modules/i18n/cache.js
@@ -0,0 +1,27 @@
+const logger = require('../logger/main.mod.js');
+
+// locale cache storage
+const cache = new Map();
+
+// get locale data from cache
+function get(locale) {
+    const cachedLocaleData = cache.get(locale);
+    return cachedLocaleData;
+}
+
+// check if cache has locale
+function has(locale) {
+    return cache.has(locale);
+}
+
+// save locale data to cache
+function save(locale, data) {
+    if (cache.has(locale)) {
+        // cache already has data for this locale.
+        logger.verbose('i18n', `Cache already has ${locale} data. Skipping.`);
+        return;
+    }
+    cache.set(locale, data);
+}
+
+module.exports = { get, has, save };
