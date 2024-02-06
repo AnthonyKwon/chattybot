9e44a32d9202ed421d381f900da8786a3b0b726d
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2024-02-07 08:38:32 +0900

    tts: remove unneeded separate globalname module

--- modules/tts/class/TTSUser.js
+++ modules/tts/class/TTSUser.js
@@ -1,44 +1,15 @@
-const { getGlobalName } = require('../../discordutils/globalname.js');
-
-// Get server username from user's ID
-const getUsername = (user, guild=undefined) => {
-    let username = undefined;
-    if (guild) {
-        // get user display name from guild
-	    username = guild.members.cache.get(user.id).displayName;
-        username = username.split('_').join(' ')
-    } else {
-        // get user display name directly
-        username = getGlobalName(user);
-    }
-    return username;
-}
-
 //TODO: change this class to type
 class TTSUserClass {
-    constructor(discordUser, guild=undefined) {
-        this._user = discordUser;
+    constructor(user) {
+        this._user = user;
         // set name field to guild displayname if guild variable is provided
         // set to global displayname if not
-        this._name = guild ? getUsername(discordUser, guild) : discordUser.name;
+        this._name = user.nickname ? user.nickname : user.displayName;
     }
 
     // (getter) get TTS user id
     get id()  { return this._user.id; }
-
-    // get TTS user name
-    async getUsername() { 
-        let username = undefined;
-        if (this._guild) {
-            // get user display name from guild
-            username = this._guild.members.cache.get(user.id).displayName;
-            username = username.split('_').join(' ')
-        } else {
-            // get user display name directly
-            username = getGlobalName(this._user);
-        }
-        return username;
-     }
+    get name()  { return this._name; }
 }
 
 module.exports = TTSUserClass;
