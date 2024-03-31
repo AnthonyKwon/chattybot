680c9b0a98539b6138c52201d35ec7994d70ee50
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2024-03-31 13:54:07 +0900

    command: overall improve /locale command

--- commands/guild_locale.js
+++ commands/guild_locale.js
@@ -8,40 +8,43 @@ async function commandHandler(interaction) {
 
     if (!newLocale) {
         // no new locale provided - print current guild locale and exit
-        interaction.editReply(i18n.get(currLocale, 'message.discord.locale.guild_current')
-            .format(currLocale));
+        interaction.editReply(i18n.get(interaction.locale, 'message.discord.locale.guild_current')
+            .format(interaction.guild.name,
+                i18n.get(interaction.locale, 'locale.name'), interaction.locale,
+                i18n.get(interaction.guild.preferredLocale, 'locale.name'), currLocale,
+            ));
         return;
     }
 
     // new locale provided - set new guild locale
     // check if user has permission to use this command
     if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
-        interaction.editReply(i18n.get(currLocale, 'error.discord.user.no_permission'));
+        interaction.editReply(i18n.get(interaction.locale, 'error.discord.user.no_permission'));
         return;
     }
 
     // check if bot has permission to update the guild settings
     if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
-        interaction.editReply(i18n.get(currLocale, 'error.discord.bot.no_permission')
-            .format(i18n.get(currLocale, 'permissions.MANAGE_GUILD')));
+        interaction.editReply(i18n.get(interaction.locale, 'error.discord.bot.no_permission')
+            .format(i18n.get(interaction.locale, 'permissions.MANAGE_GUILD')));
         return;
     }
 
     // check if user provided valid locale parameter
     if (!Object.values(Locale).includes(newLocale)) {
-        interaction.editReply(i18n.get(currLocale, 'error.discord.command.locale_invalid_params'));
+        interaction.editReply(i18n.get(interaction.locale, 'error.discord.command.locale_invalid_params'));
         return;
     }
 
     // check if server locale is the same as before
     if (interaction.guild.preferredLocale === newLocale) {
-        interaction.editReply(i18n.get(currLocale, 'error.discord.command.locale_already_set'))
+        interaction.editReply(i18n.get(interaction.locale, 'error.discord.command.locale_already_set'))
         return;
     }
 
     // set parameter as new guild locale
     interaction.guild.edit({ preferredLocale: newLocale });
-    interaction.editReply(i18n.get(newLocale, 'message.discord.locale.guild_set')
+    interaction.editReply(i18n.get(interaction.locale, 'message.discord.locale.guild_set')
         .format(currLocale, newLocale));
 }
 
