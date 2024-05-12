c5efb63e7943684ba429f3aa0ce0fb22b1da1198
AnthonyKwon <me@thonlog.com>
2024-05-12 15:37:39 +0900

    TTS: fetch username directly from discord API instead of discord.js

--- modules/tts/class/TTSUser.js
+++ modules/tts/class/TTSUser.js
@@ -1,15 +1,28 @@
+const { REST, Routes } = require('discord.js');
+const config = require('../../config.js');
+
 //TODO: change this class to type
 class TTSUserClass {
-    constructor(user) {
+    constructor(guild, user) {
+        this._guild = guild;
         this._user = user;
-        // set name field to guild displayname if guild variable is provided
-        // set to global displayname if not
-        this._name = user.nickname ? user.nickname : user.displayName;
     }
 
-    // (getter) get TTS user id
-    get id()  { return this._user.id; }
-    get name()  { return this._name; }
+    async fetchUser() {
+        return this.fetchUserRaw();
+    }
+
+    async fetchUserRaw() {
+        const rest = new REST().setToken(config.token);
+        const rawUser = await rest.get(Routes.guildMember(this._guild.id, this._user.id));
+        console.log(rawUser);
+        const user = {
+            id: rawUser.id,
+            name: rawUser.nick ? rawUser.nick :
+                rawUser.global_name ? rawUser.global_name : rawUser.username
+        }
+        return user;
+    }
 }
 
 module.exports = TTSUserClass;
