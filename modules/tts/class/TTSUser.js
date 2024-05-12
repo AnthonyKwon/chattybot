36678c60575660bb8a7489cd234fb4c21dac2c06
AnthonyKwon <me@thonlog.com>
2024-05-12 16:10:14 +0900

    TTS: use caching for TTSUser

--- modules/tts/class/TTSUser.js
+++ modules/tts/class/TTSUser.js
@@ -1,6 +1,9 @@
 const { REST, Routes } = require('discord.js');
 const config = require('../../config.js');
 
+const userCache = new Map();
+const timerCache = new Map();
+
 //TODO: change this class to type
 class TTSUserClass {
     constructor(guild, user) {
@@ -8,19 +11,44 @@ class TTSUserClass {
         this._user = user;
     }
 
-    async fetchUser() {
-        return this.fetchUserRaw();
+    fetchCacheId() { return `${this._guild}::${this._user}` }
+
+    async fetchUser(force = false) {
+        // fetch from cache if exists
+        if (!force && userCache.get(this.fetchCacheId())) return this.fetchUserCache();
+        // fetch from server if not exists
+        else return await this.fetchUserRaw();
     }
 
-    async fetchUserRaw() {
-        const rest = new REST().setToken(config.token);
-        const rawUser = await rest.get(Routes.guildMember(this._guild.id, this._user.id));
-        console.log(rawUser);
-        const user = {
-            id: rawUser.id,
-            name: rawUser.nick ? rawUser.nick :
-                rawUser.global_name ? rawUser.global_name : rawUser.username
+    // fetch user data from cache
+    fetchUserCache() {
+        return userCache.get(this.fetchCacheId());
+    }
+
+    // fetch user data from discord server
+    async fetchUserRaw(force = false) {
+        // cancel the cache purge timer (if exists)
+        if (timerCache.get(this.fetchCacheId())) {
+            clearTimeout(timerCache.get(this.fetchCacheId()));
+            timerCache.delete(this.fetchCacheId());
         }
+
+        // initialize REST API parser
+        const rest = new REST().setToken(config.token);
+        // fetch member from API
+        const rawUser = await rest.get(Routes.guildMember(this._guild.id, this._user.id));
+        // create user data object
+        const user = {
+            id: rawUser.user.id,
+            name: rawUser.nick ? rawUser.nick :
+                rawUser.user.global_name ? rawUser.user.global_name : rawUser.user.username
+        }
+
+        // save to cache for 1 day
+        const cachePurgeTimer = setTimeout(() => userCache.delete(this.fetchCacheId()), 86400000);
+        userCache.set(this.fetchCacheId(), user);
+        timerCache.set(this.fetchCacheId(), cachePurgeTimer);
+
         return user;
     }
 }
