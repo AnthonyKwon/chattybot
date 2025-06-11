874391d8a3f44399f400ad6592ee6cdd66b868eb
AnthonyKwon <12590757+AnthonyKwon@users.noreply.github.com>
2025-06-12 01:00:07 +0900

    Implemented log rotation

--- modules/logger/main.mod.js
+++ modules/logger/main.mod.js
@@ -1,9 +1,15 @@
 const path = require('path');
 const winston = require('winston');
+require('winston-daily-rotate-file');
+const config = require('../config');
 
 // set winston log level to verbose, if app is running on dev mode or slash-command related job
 const logLevel = (process.env.NODE_ENV == 'development' || process.env.SLASH_ACTION) ?
                   winston.config.syslog.levels.verbose : winston.config.syslog.levels.warning;
+
+// get log rotation time and size limit from config
+const timeLimit = `${config['log-rotate'].timeLimit ?? 24}h`;
+const sizeLimit = `${config['log-rotate'].sizeLimit ?? 100}k`;
 
 // create new logger object and export it
 module.exports = winston.createLogger({
@@ -21,9 +27,16 @@ module.exports = winston.createLogger({
             winston.format.printf(info => `${info.timestamp} - ${info.level}/${info.topic}:  ${info.message}`)),
         level: logLevel
         }),
-        new winston.transports.File({
-            filename: path.join(__dirname, '../../logs/latest.log'),
-            level: logLevel
+        new winston.transports.DailyRotateFile({
+            filename: path.join(__dirname, '../../logs/%DATE%'),
+            dirname: path.join(__dirname, '../../logs'),
+            extension: '.log',
+            datePattern: 'YYYY-MM-DD',
+            frequency: timeLimit,
+            maxSize: sizeLimit,
+            createSymlink: true,
+            symlinkName: 'latest.log',
+            level: logLevel,
         })
     ]
 });
