c749bffb275323f59f97e200c32577a05e8cc81c
AnthonyKwon <me@thonlog.com>
2025-09-18 07:16:09 +0900

    Rewrite main into TypeScript.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { ConversationManager } = require('./modules/conversation/Conversation');
-const command = require('./modules/discord/command/Command');
-const config = require('./modules/config/ConfigLoader').default;
-const logger = require('./modules/log/Logger').default;
-const appInfo = require('../package.json');
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { ConversationManager } from './modules/conversation/Conversation';
+import * as command from './modules/discord/command/Command';
+import config from './modules/config/ConfigLoader';
+import logger from './modules/log/Logger';
 
-// initialize logger_legacy module for main
+// get package information from package.json
+const appInfo = JSON.parse(readFileSync(resolve(global.appRoot, 'package.json'), 'utf8'));
+
+// initialize logger module for main
 logger.info({ topic: appInfo.name, message: `version ${appInfo.version}` });
 
 
-// create discord_legacy.js client object
+// create discord client object
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -21,10 +25,10 @@ const client = new Client({
 
 client.once(Events.ClientReady, async c => {
     // create voice session map
-    logger.info({ topic: 'discord', message: `Connected to ${client.user.username}!` });
+    logger.info({ topic: 'discord', message: `Connected to ${client.user?.username ?? 'Discord user'}!` });
 
     // set bot activity message if available
-    if (config.status && config.status !== '') c.user.setActivity(config.status);
+    if (config.discord.status) c.user.setActivity(config.discord.status);
 
     // load registered slash commands
     await command.load();
@@ -79,40 +83,40 @@ client.on(Events.ThreadUpdate, (oldThread, newThread) => {
         const conversation = ConversationManager.get(newThread.guildId);
 
         // ignore the event not related to conversation
-        if (!conversation) return;
+        if (!conversation || conversation.destroyed ||
+            !conversation.verify(newThread.guildId, newThread.id)) return;
 
         // emit conversation thread deleted event
         conversation.emit(`threadObsolete-${newThread.guildId}`);
     }
 });
 
-// log unhandled error of discord_legacy.js
+// log unhandled error of discord.js
 client.on(Events.Error, err => {
-    logger.error({ topic: 'discord_legacy.js', message: 'Unhandled error has occured!' });
-    logger.error({ topic: 'discord_legacy.js', message: err.stack });
+    logger.error({ topic: 'discord', message: 'Unhandled error has occurred!' });
+    logger.error({ topic: 'discord', message: err.stack });
 });
 
 // exit with error on unhandled Rejection
-process.on('unhandledRejection', err => {
-    logger.error({ topic: appInfo.name, message: 'An unhandled Rejection has occured, appliation will exit!' });
+process.on('unhandledRejection', (err: Error) => {
+    logger.error({ topic: appInfo.name, message: 'An unhandled Rejection has occurred, application will exit!' });
     logger.error({ topic: appInfo.name, message: err.stack });
     process.exit(1);
 });
 
 // exit with error on uncaught Exception
-process.on('uncaughtException', err => {
-    logger.error({ topic: appInfo.name, message: 'An uncaught Exception has thrown, appliation will exit!' });
+process.on('uncaughtException', (err: Error) => {
+    logger.error({ topic: appInfo.name, message: 'An uncaught Exception has thrown, application will exit!' });
     logger.error({ topic: appInfo.name, message: err.stack });
     process.exit(1);
 });
 
-// destroy discord_legacy.js connection on exit
+// destroy discord.js connection on exit
 function exitHandler() {
     client.destroy().finally(() => process.exit(0));
 }
 process.on('SIGINT', exitHandler);
 process.on('SIGTERM', exitHandler);
 
-// initialize discord_legacy module
+// initialize discord module
 client.login(config.discord.token);
-//require('./modules/discord/command/CommandRegister').register();
