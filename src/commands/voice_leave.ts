0a1d13c632c6da8fe1797ae5dbc73ff98ffa40dd
AnthonyKwon <me@thonlog.com>
2025-09-18 21:51:33 +0900

    command: rewrite 'empty' and 'leave' into typescript.

---
+++ src/commands/voice_leave.ts
@@ -0,0 +1,32 @@
+import {CommandInteraction, VoiceChannel} from 'discord.js';
+import { ConversationManager } from '../modules/conversation/Conversation';
+import { getString } from '../modules/i18n/GetString';
+import I18nCommandBuilder from "../modules/discord/command/I18nCommandBuilder";
+
+async function commandHandler(interaction: CommandInteraction) {
+    const conversation: ConversationManager | undefined = ConversationManager.get(interaction.guild!.id);
+
+    // check if bot has joined session
+    if (!conversation) {
+        await interaction.editReply(getString(interaction.locale, 'error.botNotInVC'));
+        return;
+    }
+
+    // get conversation from cache and leave
+    const channel: VoiceChannel = await conversation.destroy();
+
+    // check channel availability before sending reply
+    // (in case of user sending command to destroying conversation)
+    if (!interaction.channel) return;
+
+    // send reply to user interaction
+    await interaction.editReply(getString(interaction.locale, 'message.conversation.left', channel.toString()));
+}
+
+module.exports = {
+    data: new I18nCommandBuilder('leave')
+        .setName()
+        .setDescription(),
+    ephemeral: true,
+    execute: commandHandler
+}
